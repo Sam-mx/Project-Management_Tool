2c915fa38697f91fe588cc41c089d0f09d238892
San Shwe <sanxhwe@gmail.com>
2025-05-30 12:02:42 +0800

    types

---
+++ server/types/constants.ts
@@ -0,0 +1,45 @@
+export const BOARD_VISIBILITY = {
+  PRIVATE: "PRIVATE",
+  PUBLIC: "PUBLIC",
+};
+
+// space member roles
+export const SPACE_MEMBER_ROLES = {
+  ADMIN: "ADMIN",
+  NORMAL: "NORMAL",
+  GUEST: "GUEST",
+};
+
+// board member roles
+export const BOARD_MEMBER_ROLES = {
+  ADMIN: "ADMIN",
+  NORMAL: "NORMAL",
+  OBSERVER: "OBSERVER",
+};
+
+// favorite obj types
+export const SPACE = "SPACE";
+export const BOARD = "BOARD";
+
+// LIST POSSIBLE DRAGS
+export const LIST_POSSIBLE_DRAGS = {
+  LEFT: "LEFT",
+  RIGHT: "RIGHT",
+};
+
+// CARD POSSIBLE DRAGS
+export const CARD_POSSIBLE_DRAGS = {
+  DOWN: "DOWN",
+  UP: "UP",
+};
+
+// LABEL COLORS
+export const LABEL_COLORS = {
+  GREEN: "#61BD4F",
+  YELLOW: "#F1D737",
+  ORANGE: "#FA9F2E",
+  RED: "#EB5A46",
+  PURPLE: "#C377E0",
+  BLUE: "#0079BF",
+  LIGHTBLUE: "#3BC3E0",
+};
